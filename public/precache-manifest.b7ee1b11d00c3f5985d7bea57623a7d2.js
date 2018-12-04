9b138e3d4a374d980422d6d6269c8a0ec275b1b8
Faycel Benyoussa <faysou@laposte.net>
2018-12-04 20:43:52 +0100

    sample app using gql

---
+++ public/precache-manifest.b7ee1b11d00c3f5985d7bea57623a7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdaab9ad61ce39cd14a5f7996e3c6faf",
+    "url": "/static/media/logo.fdaab9ad.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e731574be929cf7ac2b1",
+    "url": "/static/js/main.e731574b.chunk.js"
+  },
+  {
+    "revision": "d92f2688271638f32685",
+    "url": "/static/js/1.d92f2688.chunk.js"
+  },
+  {
+    "revision": "e731574be929cf7ac2b1",
+    "url": "/static/css/main.e6858b32.chunk.css"
+  },
+  {
+    "revision": "db351c3864481f3e0e947de096d0567f",
+    "url": "/index.html"
+  }
+];
